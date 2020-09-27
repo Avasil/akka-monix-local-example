dab1b6422d86153fa985c3e8e3fde85c743f18ff
piotrgawrys <pgawrys2@gmail.com>
2020-09-27 18:04:05 +0200

    Update example

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-val monixVersion      = "3.2.0-4524fe2-SNAPSHOT"
+val monixVersion      = "3.2.2+49-d027e5f7-SNAPSHOT"
 val akkaHttpVersion   = "10.1.11"
 val akkaStreamVersion = "2.6.3"
 
@@ -11,7 +11,6 @@ libraryDependencies := Seq(
   "com.typesafe.akka"          %% "akka-http"      % akkaHttpVersion,
   "com.typesafe.akka"          %% "akka-stream"    % akkaStreamVersion,
   "io.monix"                   %% "monix"          % monixVersion,
-  "org.mdedetrich"             %% "monix-mdc"      % "0.1.0-SNAPSHOT",
   "ch.qos.logback"             % "logback-classic" % "1.2.3",
   "com.typesafe.scala-logging" %% "scala-logging"  % "3.9.2"
 )
--- src/main/scala/AkkaHTTPExample.scala
+++ src/main/scala/AkkaHTTPExample.scala
@@ -4,12 +4,9 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
-import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.StrictLogging
 import monix.eval.Task
 import monix.execution.Scheduler
-import monix.execution.misc.Local
-import org.mdedetrich.monix.mdc.MonixMDCAdapter
 import org.slf4j.MDC
 
 import scala.compat.java8.OptionConverters._
@@ -17,20 +14,18 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.util.{Random, Try}
 
-object AkkaHttpTest extends App with StrictLogging {
+object AkkaHTTPExample extends App with StrictLogging {
   MonixMDCAdapter.initialize()
-  implicit val actorSystem: ActorSystem        = ActorSystem()
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-  implicit val scheduler: Scheduler            = Scheduler.traced
-  implicit val opts: Task.Options              = Task.defaultOptions.enableLocalContextPropagation.disableLocalContextIsolateOnRun
+  implicit val actorSystem: ActorSystem = ActorSystem()
+  implicit val scheduler: Scheduler     = Scheduler.traced
 
-  val addResponseHeader: Directive0 =
+  val addCorrelationIdHeader: Directive0 =
     mapInnerRoute(_.andThen(_.flatMap {
       case result @ RouteResult.Complete(response) =>
         Future {
-          val l = MDC.get("local")
+          val l = MDC.get("correlationId")
           if (l != null)
-            RouteResult.Complete(response.addHeader(RawHeader("local", l)))
+            RouteResult.Complete(response.addHeader(RawHeader("correlationId", l)))
           else
             result
         }
@@ -40,21 +35,17 @@ object AkkaHttpTest extends App with StrictLogging {
         }
     }))
 
-  def onCompleteLocal[T](task: => Task[T]): Directive1[Try[T]] = {
-    import akka.http.scaladsl.util.FastFuture._
+  def onCompleteTask[T](task: => Task[T]): Directive1[Try[T]] =
     Directive { inner => ctx =>
-      task.runToFutureOpt.fast.transformWith(t => inner(Tuple1(t))(ctx))
+      task.runToFuture.transformWith(t => inner(Tuple1(t))(ctx))
     }
-  }
 
   def routes: Route =
     path("hello" / IntNumber) { id =>
       get {
-        addResponseHeader {
-          onCompleteLocal(Task {
-            Local.isolate {
-              MDC.put("local", id.toString)
-            }
+        addCorrelationIdHeader {
+          onCompleteTask(Task {
+            MDC.put("correlationId", id.toString)
           }) { _ =>
             complete(StatusCodes.OK)
           }
@@ -62,7 +53,7 @@ object AkkaHttpTest extends App with StrictLogging {
       }
     }
 
-  val serverBinding = Await.result(Http(actorSystem).bindAndHandle(routes, "localhost", 8080), 1 minutes)
+  val serverBinding = Await.result(Http(actorSystem).bindAndHandle(routes, "localhost", 8080), 1.minutes)
 
   val http = Http()
 
@@ -74,15 +65,15 @@ object AkkaHttpTest extends App with StrictLogging {
                    HttpMethods.GET,
                    Uri(s"http://localhost:8080/hello/$int")
                  ))
-      header = result.getHeader("local").asScala
+      header = result.getHeader("correlationId").asScala
       output = s"input $int: output: ${header.map(_.value()).getOrElse("no header")}"
       _      = result.discardEntityBytes()
       _      = println(output)
     } yield ()
   }
 
-  Await.result(Future.sequence(futures), 1 minutes)
-  Await.result(serverBinding.terminate(1 minutes), 1 minutes)
+  Await.result(Future.sequence(futures), 1.minutes)
+  Await.result(serverBinding.terminate(1.minutes), 1.minutes)
 
   System.exit(0)
 }
